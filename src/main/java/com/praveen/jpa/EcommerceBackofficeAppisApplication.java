147931cdbf2288e8ecd35379916fafaef3c0ee33
Praveen Sana <spraveen0244@gmail.com>
2022-11-20 20:01:18 +0530

    ecommerce backoffice apis

---
+++ src/main/java/com/praveen/jpa/EcommerceBackofficeAppisApplication.java
@@ -0,0 +1,13 @@
+package com.praveen.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBackofficeAppisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBackofficeAppisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/praveen/jpa/EcommerceBackofficeAppisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.praveen.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceBackofficeAppisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
