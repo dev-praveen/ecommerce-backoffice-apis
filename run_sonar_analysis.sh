636c17518c883b7e126c7a2ec9213a07f14ff3c4
dev-praveen <spraveen0244@gmail.com>
2024-03-23 09:17:24 +0530

    refactor[ops]: removed compose.yml and run_sonar_analysis.sh file

--- run_sonar_analysis.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mvn clean verify sonar:sonar \
-  -Dsonar.projectKey=ecommerce-sonar \
-  -Dsonar.projectName='ecommerce-sonar' \
-  -Dsonar.host.url=http://localhost:9000 \
-  -Dsonar.token=sqp_b2d08c5d82e6a6d94a3be9960c939fcddc94fa43
