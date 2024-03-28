7e947e39588d1af650e7efeca52dc703b8caf52f
dev-praveen <spraveen0244@gmail.com>
2024-03-28 16:14:35 +0530

    refactor[exception]: removed unused methods in exception classes

--- src/main/java/com/praveen/jpa/exception/CustomerNotFoundException.java
+++ src/main/java/com/praveen/jpa/exception/CustomerNotFoundException.java
@@ -2,19 +2,7 @@ package com.praveen.jpa.exception;
 
 public class CustomerNotFoundException extends RuntimeException {
 
-  public CustomerNotFoundException() {
-    super();
-  }
-
   public CustomerNotFoundException(String message) {
     super(message);
-  }
-
-  public CustomerNotFoundException(Throwable throwable) {
-    super(throwable);
-  }
-
-  public CustomerNotFoundException(String message, Throwable throwable) {
-    super(message, throwable);
   }
 }
--- src/main/java/com/praveen/jpa/exception/DuplicateCustomerException.java
+++ src/main/java/com/praveen/jpa/exception/DuplicateCustomerException.java
@@ -2,19 +2,7 @@ package com.praveen.jpa.exception;
 
 public class DuplicateCustomerException extends RuntimeException {
 
-  public DuplicateCustomerException() {
-    super();
-  }
-
   public DuplicateCustomerException(String message) {
     super(message);
-  }
-
-  public DuplicateCustomerException(Throwable throwable) {
-    super(throwable);
-  }
-
-  public DuplicateCustomerException(String message, Throwable throwable) {
-    super(message, throwable);
   }
 }
