4efff9b0040c361c3f3eec9e98c3724a2c2d2c43
dev-praveen <spraveen0244@gmail.com>
2025-12-30 09:19:02 +0530

    refactor[auth]: introduced response entity for auth response

--- src/main/java/com/praveen/jpa/resource/AuthController.java
+++ src/main/java/com/praveen/jpa/resource/AuthController.java
@@ -14,6 +14,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -36,7 +37,7 @@ public class AuthController {
       tags = {"jwt-resource"},
       responses = {
         @ApiResponse(
-            responseCode = "201",
+            responseCode = "200",
             description = "Created",
             content = {
               @Content(
@@ -46,7 +47,7 @@ public class AuthController {
       })
   @PostMapping("/token")
   @SecurityRequirements
-  public TokenResponse generateToken(
+  public ResponseEntity<TokenResponse> generateToken(
       @Parameter(name = "TokenRequest", required = true) @Valid @RequestBody
           TokenRequest tokenRequest) {
 
@@ -54,6 +55,7 @@ public class AuthController {
     UsernamePasswordAuthenticationToken authRequest =
         new UsernamePasswordAuthenticationToken(tokenRequest.userName(), tokenRequest.password());
     Authentication authentication = authenticationManager.authenticate(authRequest);
-    return tokenService.generateToken(authentication);
+    final var tokenResponse = tokenService.generateToken(authentication);
+    return ResponseEntity.ok(tokenResponse);
   }
 }
